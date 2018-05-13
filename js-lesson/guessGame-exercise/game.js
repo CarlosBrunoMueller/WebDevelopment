f02a83a2337531d3e79f0ac299de9a397071801b
CarlosMueller <cbmuellerjr@gmail.com>
2018-05-13 14:38:55 -0300

    Guess game Exercise

---
+++ js-lesson/guessGame-exercise/game.js
@@ -0,0 +1,19 @@
+//Create Secret Number
+var secretNumber = 4;
+
+//ask user for guess
+var guess = Number(prompt("Guess a number"));
+
+
+//check guess
+if (guess === secretNumber) {
+    alert("You got it!!");
+}
+else if (guess < secretNumber) {
+    alert("To low. Guess again");
+}
+else {
+    alert("To high. Guess again");
+}
+
+
